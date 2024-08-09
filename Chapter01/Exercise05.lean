47f8702fd72d7814212ed2c137954f1ee1c4a80c
Kate Novak <kje@cochleoid.com>
2024-08-09 06:34:34 -0400

    Chapter 1, Exercise 5

--- Chapter01/Exercise05.lean
+++ Chapter01/Exercise05.lean
@@ -27,17 +27,29 @@ theorem MunkresCh1Ex5PartA1 {U} [h : Nontrivial U]
   assumption
   assumption
 
--- This statement
+-- The statement
 --   ∃ A ∈ AA, x ∈ ⋃₀ AA → x ∈ A
 -- has the converse
 --   ∃ A ∈ AA, x ∈ A → x ∈ ⋃₀ AA
 -- which is not generally true. If AA is empty, then there is no such A to
--- satisfy the statement. We work with an equivalent statement
+-- satisfy the statement. The statement can also be written
 --   ∃ A ∈ AA, A ⊆ ⋃₀ AA
-theorem MunkresCh1Ex5PartA2 {U} (AA : Set (Set U))
-    : ∃ A ∈ AA, A ⊆ ⋃₀ AA := by
-  sorry
+-- which we do here.
+theorem MunkresCh1Ex5PartA2 {U}
+    : ∃ AA : Set (Set U), ¬(∃ A ∈ AA, A ⊆ ⋃₀ AA) := by
+  use {}
+  simp
 
+-- However, if AA is not empty, then the converse is true.
+theorem MunkresCh1Ex5PartA2_2 {U} (AA : Set (Set U)) [h : Nonempty AA]
+    : ∃ A ∈ AA, A ⊆ ⋃₀ AA := by
+  obtain ⟨A, AAA⟩ := h
+  use A
+  constructor
+  assumption
+  intro x xa
+  simp
+  use A
 
 -- The statement
 --   ∀ A ∈ AA, x ∈ ⋃₀ AA → x ∈ A
@@ -46,7 +58,7 @@ theorem MunkresCh1Ex5PartA2 {U} (AA : Set (Set U))
 -- The statement can also be written
 --   ∀ A ∈ AA, ⋃₀ AA ⊆ A
 -- which we do here.
-theorem MunkresCh1Ex5PartB {U} [h : Nontrivial U]
+theorem MunkresCh1Ex5PartB1 {U} [h : Nontrivial U]
     : ∃ AA : Set (Set U), ¬(∀ A ∈ AA, ⋃₀ AA ⊆ A) := by
   obtain ⟨a, ⟨b, anb⟩⟩ := h
   use {{a}, {b}}
@@ -56,12 +68,26 @@ theorem MunkresCh1Ex5PartB {U} [h : Nontrivial U]
   assumption
 
 -- The statement
+--   ∀ A ∈ AA, x ∈ ⋃₀ AA → x ∈ A
+-- has the converse
+--   ∀ A ∈ AA, x ∈ A → x ∈ ⋃₀ AA
+-- which is true. This can be written as
+--   ∀ A ∈ AA, A ⊆ ⋃₀ AA
+-- which we do here.
+theorem MunkresCh1Ex5PartB2 {U} (AA : Set (Set U))
+    : ∀ A ∈ AA, A ⊆ ⋃₀ AA := by
+  intro A AAA
+  intro x xA
+  simp
+  use A
+
+-- The statement
 --   ∃ A ∈ AA, x ∈ ⋂₀ AA → x ∈ A
 -- is true when AA is nonempty. When AA is empty, the statement is not true
 -- since no A ∈ AA exists. We work with the equivalent statement for the case
 -- where AA is non-empty.
 --   ∃ A ∈ AA, ⋂₀ AA ⊆ A.
-theorem MunkresCh1Ex5PartC {U} (AA : Set (Set U)) [h : Nonempty AA]
+theorem MunkresCh1Ex5PartC1 {U} (AA : Set (Set U)) [h : Nonempty AA]
     : ∃ A ∈ AA, ⋂₀ AA ⊆ A := by
   obtain ⟨A, h2⟩ := h
   use A
@@ -72,12 +98,48 @@ theorem MunkresCh1Ex5PartC {U} (AA : Set (Set U)) [h : Nonempty AA]
   assumption
 
 -- The statement
+--   ∃ A ∈ AA, x ∈ ⋂₀ AA → x ∈ A
+-- has the converse
+--   ∃ A ∈ AA, x ∈ A → x ∈ ⋂₀ AA
+-- which is not generally true. If AA = {{x}, {y}} for distinct x and y, then
+-- ⋂₀ AA = {} and none of {x} or {y} are subsets of it. We work with the
+-- equivalent statement
+--   ∃ A ∈ AA, A ⊆ ⋂₀ AA
+theorem MunkresCh1Ex5PartC2 {U} (h : Nontrivial U)
+    : ∃ AA : Set (Set U), ¬(∃ A ∈ AA, A ⊆ ⋂₀ AA) := by
+  obtain ⟨x, ⟨y, xny⟩⟩ := h
+  use {{x}, {y}}
+  simp
+  push_neg
+  constructor
+  assumption
+  apply Ne.symm
+  assumption
+
+-- The statement
 --   ∀ A ∈ AA, x ∈ ⋂₀ AA → x ∈ A
 -- is true. We work with the equivalent statement
 --   ∀ A ∈ AA, ⋂₀ AA ⊆ A.
-theorem MunkresCh1Ex5PartD {U} (AA : Set (Set U))
+theorem MunkresCh1Ex5PartD1 {U} (AA : Set (Set U))
     : ∀ A ∈ AA, ⋂₀ AA ⊆ A := by
   intro A AAA
   intro x xNAA
   apply xNAA at AAA
   assumption
+
+-- The statement
+--   ∀ A ∈ AA, x ∈ ⋂₀ AA → x ∈ A
+-- has the converse
+--   ∀ A ∈ AA, x ∈ A → x ∈ ⋂₀ AA
+-- which is not generally true. If AA = {{x}, {y}} for distinct x and y, then
+-- ⋂₀ AA = {} and none of {x} or {y} are subsets of it. We work with the
+-- equivalent statement
+--   ∀ A ∈ AA, A ⊆ ⋂₀ AA
+theorem MunkresCh1Ex5PartD2 {U} [h : Nontrivial U]
+    : ∃ AA : Set (Set U), ¬(∀ A ∈ AA, A ⊆ ⋂₀ AA) := by
+  obtain ⟨x, ⟨y, xny⟩⟩ := h
+  use {{x}, {y}}
+  push_neg
+  simp
+  push_neg
+  exact Or.inl xny
